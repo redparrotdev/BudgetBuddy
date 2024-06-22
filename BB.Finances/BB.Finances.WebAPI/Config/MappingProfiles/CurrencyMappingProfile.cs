91126534f40012bf511b938df8ecf97c28c1c462
kr.socialmail@gmail.com <kr.socialmail@gmail.com>
2024-06-22 12:07:45 +0300

    Added Currency Controller

--- BB.Finances/BB.Finances.WebAPI/Config/MappingProfiles/CurrencyMappingProfile.cs
+++ BB.Finances/BB.Finances.WebAPI/Config/MappingProfiles/CurrencyMappingProfile.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using BB.Finances.Data.DTO;
 using BB.Finances.Data.Entities;
+using BB.Finances.WebAPI.Models.Request;
+using BB.Finances.WebAPI.Models.Response;
 
 namespace BB.Finances.WebAPI.Config.MappingProfiles
 {
@@ -13,10 +15,20 @@ namespace BB.Finances.WebAPI.Config.MappingProfiles
                 .ForMember(dto => dto.Name, opt => opt.MapFrom(c => c.Name))
                 .ForMember(dto => dto.CurrencySign, opt => opt.MapFrom(c => c.CurrencySign));
 
-            CreateMap<CurrencyDTO, CurrencyDTO>()
+            CreateMap<CurrencyDTO, Currency>()
                 .ForMember(c => c.Id, opt => opt.MapFrom(dto => dto.Id))
                 .ForMember(c => c.Name, opt => opt.MapFrom(dto => dto.Name))
                 .ForMember(c => c.CurrencySign, opt => opt.MapFrom(dto => dto.CurrencySign));
+
+            CreateMap<CurrencyDTO, CurrencyResponseModel>()
+                .ForMember(rm => rm.Id, opt => opt.MapFrom(dto => dto.Id))
+                .ForMember(rm => rm.Name, opt => opt.MapFrom(dto => dto.Name))
+                .ForMember(rm => rm.CurrencySign, opt => opt.MapFrom(dto => dto.CurrencySign));
+
+            CreateMap<CurrencyRequestModel, CurrencyDTO>()
+                .ForMember(dto => dto.Id, opt => Guid.NewGuid())
+                .ForMember(dto => dto.Name, opt => opt.MapFrom(rm => rm.Name))
+                .ForMember(dto => dto.CurrencySign, opt => opt.MapFrom(rm => rm.CurrencySign));
         }
     }
 }
---
+++ BB.Finances/BB.Finances.WebAPI/Models/Response/CurrencyResponseModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BB.Finances.WebAPI.Models.Response
+{
+    public class CurrencyResponseModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string CurrencySign { get; set; }
+    }
+}
