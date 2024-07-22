7e3da953ddc2ffa590d80d0c5fba8ec856906600
redparrotdev <kurilenko.roman.dev@gmail.com>
2024-07-22 12:53:09 +0300

    Added Jwt CQRS. Test REfactored to suit new controller constructor

---
+++ BB.IdentityService/BB.Core/CQRS/JWT/CreateJWT.cs
@@ -0,0 +1,60 @@
+﻿using MediatR;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BB.Core.CQRS.JWT
+{
+    public class CreateJWT : IRequest<string>
+    {
+        public Guid UserId { get; set; }
+
+        public string Email { get; set; }
+
+        public string Issuer { get; set; }
+
+        public string Audience { get; set; }
+
+        public DateTime Expires { get; set; }
+
+        public SigningCredentials Credentials { get; set; }
+    }
+
+    public class CreateJWTHandler : IRequestHandler<CreateJWT, string>
+    {
+
+        public async Task<string> Handle(CreateJWT request, CancellationToken cancellationToken)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(request.Email);
+            ArgumentException.ThrowIfNullOrEmpty(request.Issuer);
+            ArgumentException.ThrowIfNullOrEmpty(request.Audience);
+
+            if (request.UserId.Equals(Guid.Empty))
+                throw new ArgumentException(nameof(request.UserId));
+
+            var claims = new List<Claim>
+            {
+                new Claim("UserId", request.UserId.ToString()),
+                new Claim("Email", request.Email)
+            };
+
+            var jwt = new JwtSecurityToken(
+                issuer: request.Issuer,
+                audience: request.Audience,
+                claims: claims,
+                expires: request.Expires,
+                signingCredentials: request.Credentials);
+
+            var handler = new JwtSecurityTokenHandler();
+
+            var token = handler.WriteToken(jwt);
+
+            return await Task.FromResult(token);
+        }
+    }
+}
---
+++ BB.IdentityService/BB.IdentityService.Tests/Fixtures/JwtTokenFixture.cs
@@ -0,0 +1,41 @@
+﻿using BB.IdentityService.WebAPI.Helpers;
+using BB.IdentityService.WebAPI.Models.Request;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BB.IdentityService.Tests.Fixtures
+{
+    public static class JwtTokenFixture
+    {
+        public static string CreateNewJwtToken(IConfiguration config, UserRequestModel model)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim("Name", Guid.NewGuid().ToString()),
+                new Claim("Email", model.Email)
+            };
+
+            var credentials = new SigningCredentials(
+                    JWTHelper.GetSymmetricSecurityKey(config["JWT:Key"]!),
+                    SecurityAlgorithms.HmacSha256);
+
+            var jwt = new JwtSecurityToken(
+                issuer: config["JWT:Issuer"],
+                audience: config["JWT:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(24),
+                signingCredentials: credentials);
+
+            var handler = new JwtSecurityTokenHandler();
+
+            return handler.WriteToken(jwt);
+        }
+    }
+}
